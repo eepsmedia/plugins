63d910097fd1fb91d3f27f338a170f3ca7ae38df
Tim Erickson <customer@eeps.com>
2019-06-11 14:35:47 -0700

    blodgett 001c. Fixed hour = 24 problem

--- blodgett/blodgett.js
+++ blodgett/blodgett.js
@@ -35,7 +35,7 @@ See the wiki for how we handle the data, especially time attributes: https://git
 let blodgett = {
 
     constants : {
-        version : "000b",
+        version : "000c",
         kDefaultDate : "2017-09-10",
         kBlodgettDataSetName : "blodgett",
         kBlodgettCollectionName : "blodgett",
@@ -84,18 +84,21 @@ let blodgett = {
         iData.forEach(c => {     //  c is a database case, an OBJECT
             //  compute when
             const dateOnly = new Date(c.Date);
-            const adjustedParsedDateTime = dateOnly.getTime() +
+            const adjustedParsedDateTime =
+                dateOnly.getTime() +
                 60000 * dateOnly.getTimezoneOffset() +
                 3600000 * c.Hour +
                 60000 * c.Minute;
             const adjustedDateTime = new Date(adjustedParsedDateTime);
             const stringDate = adjustedDateTime.ISO_8601_string();
+            const adjustedHours = adjustedDateTime.getHours();
+            const adjustedMinutes = adjustedDateTime.getMinutes();
 
             let out = {
                 "sample" : blodgett.state.sampleNumber,
                 "when" : adjustedDateTime,
                 "stringDate" : adjustedDateTime.toDateString(),
-                "hour" : c.Hour + c.Minute/60
+                "hour" : adjustedHours + adjustedMinutes/60
             };
             for (const key in c) {
                 if (c.hasOwnProperty(key)) {
