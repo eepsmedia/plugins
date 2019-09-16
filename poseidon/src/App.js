da7bce65c18fffe33cdc2492016341dd6e071b9f
Tim Erickson <customer@eeps.com>
2019-09-16 10:02:05 -0700

    poseidon, more react files, prep for branch

---
+++ poseidon/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//  import logo from './logo.svg';
+import './App.css';
+import Poseidon from './Poseidon.js';
+
+function App() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Poseidon testing</h1>
+{/*
+                <img src={logo} className="App-logo" alt="logo"/>
+*/}
+                <Poseidon/>
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
